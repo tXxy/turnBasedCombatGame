fd4aa999adeeca9aba5b42bd87c9436fc1ec050e
tXxy <xxyxxyyyx@gmail.com>
2020-11-30 18:27:46 +0100

    Initial commit

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour {
+
+    private void Start() {
+        Debug.Log("GameHandler.Start");
+
+        //int i = 0;
+        //FunctionPeriodic.Create(() => {
+            //CMDebug.TextPopupMouse("Ding "+i+"!");
+            //i++;
+        //}, .5f);
+    }
+
+    private void Update() {
+        
+    }
+
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid {
+
+    private int width;
+    private int height;
+    private float cellSize;
+    private int [,] gridArray;
+
+    public Grid (int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridArray = new int[width, height];
+
+        for (int x=0; x<gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                //Debug.Log(x + " " + y);
+                CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y), 20, Color.white, TextAnchor.MiddleCenter);
+            }
+        }
+    }
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize;
+    }
+
+
+    //DIESE SACHEN EXTERN AUSLAGERN
+    public const int sortingOrderDefault = 5000;
+
+    // Get Sorting order to set SpriteRenderer sortingOrder, higher position = lower sortingOrder
+    public static int GetSortingOrder(Vector3 position, int offset, int baseSortingOrder = sortingOrderDefault)
+    {
+        return (int)(baseSortingOrder - position.y) + offset;
+    }
+    public static TextMesh CreateWorldText(string text, Transform parent = null, Vector3 localPosition = default(Vector3), int fontSize = 40, Color? color = null, TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = sortingOrderDefault)
+    {
+        if (color == null) color = Color.white;
+        return CreateWorldText(parent, text, localPosition, fontSize, (Color)color, textAnchor, textAlignment, sortingOrder);
+    }
+
+    // Create Text in the World
+    public static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment, int sortingOrder)
+    {
+        GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
+        Transform transform = gameObject.transform;
+        transform.SetParent(parent, false);
+        transform.localPosition = localPosition;
+        TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.anchor = textAnchor;
+        textMesh.alignment = textAlignment;
+        textMesh.text = text;
+        textMesh.fontSize = fontSize;
+        textMesh.color = color;
+        textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
+        return textMesh;
+    }
+}

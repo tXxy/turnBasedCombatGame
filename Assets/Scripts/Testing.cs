8b13410ef882e979ef3d84b224193e7dbbd173fd
tXxy <xxyxxyyyx@gmail.com>
2020-12-01 09:35:45 +0100

    basicGridFinished

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -1,12 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Xxy.Utils;
+
 
 public class Testing : MonoBehaviour
 {
+    private Grid grid;
 
     private void Start()
     {
-        Grid grid = new Grid(4, 2, 10f);
+        grid = new Grid(4, 2, 10f, new Vector3(20, 0));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClasses.GetMouseWorldPosition(), 56);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClasses.GetMouseWorldPosition()));
+        }
     }
 }
--- Assets/Scripts/UtilsClasses.cs
+++ Assets/Scripts/UtilsClasses.cs
@@ -44,6 +44,27 @@ namespace Xxy.Utils
             return textMesh;
         }
 
+        // Get Mouse Position in World with Z = 0f
+        public static Vector3 GetMouseWorldPosition()
+        {
+            Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+            vec.z = 0f;
+            return vec;
+        }
+        public static Vector3 GetMouseWorldPositionWithZ()
+        {
+            return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        }
+        public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+        {
+            return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+        }
+        public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+        {
+            Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+            return worldPosition;
+        }
+
     }
 
 }
